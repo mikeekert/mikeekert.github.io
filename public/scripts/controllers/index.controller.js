1e7c9fdd542ecb4c6b923e38c98680ae1804754e
Mike Ekert <mikeekert@gmail.com>
2017-11-30 18:25:18 -0600

    more stuff

--- public/scripts/controllers/index.controller.js
+++ public/scripts/controllers/index.controller.js
@@ -1,6 +1,17 @@
 myApp
     .controller('UserController', function (IndexService) {
         console.log('UserController created');
-        var vm = this;
+        let vm = this;
 
+        let image_week4 = ['image0', 'image1', 'image2'];
+        let image_week5 = ['image0', 'image1'];
+        let image_week6 = ['image0', 'image1'];
+        let image_solo = [
+            'image0',
+            'image1',
+            'image2',
+            'image3',
+            'image4',
+            'image5'
+        ];
     });
