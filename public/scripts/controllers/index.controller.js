c973051b7fc011333eeac2d9c83d3383b61a9050
Mike Ekert <mikeekert@gmail.com>
2017-12-07 18:12:37 -0600

    lots of work

--- public/scripts/controllers/index.controller.js
+++ public/scripts/controllers/index.controller.js
@@ -2,5 +2,72 @@ myApp
     .controller('UserController', function (UserService) {
         console.log('UserController created');
         let vm = this;
+        vm.move = false;
 
+        vm.fadeIn = (i) => {
+            i.appClass = true;
+        };
+        vm.fadeOut = (i) => {
+            i.appClass = false;
+        };
+        vm.moveDiv = (i) => {
+            vm.move = !vm.move;
+            i.featured = true;
+            vm.showAll = false;
+            console.log(vm.articleArrays);            
+        };
+        vm.closeDiv = (i) => {
+            vm.move = !vm.move;
+            i.featured = false;
+            vm.showAll = true;
+        };
+
+        vm.articleArrays = [
+            {
+                'name': 'Image Gallery',
+                'week': '4',
+                'link': 'https://github.com/mikeekert/Weekend4-ImageGallery',
+                'image': '/assets/images/week4/image0.png',
+                'desc': 'Created Image Gallery of favorite moments, complete with db hosted Comments, and' +
+                        ' Likes.',
+                'featured': null
+            }, {
+                'name': 'Real Estate',
+                'week': '5',
+                'link': 'https://github.com/mikeekert/Weekend5-RealEstate',
+                'image': '/assets/images/week5/image0.png',
+                'desc': 'Sample Real Estate listing application, with ability to sort by types and add li' +
+                        'stings.',
+                'featured': null
+            }, {
+                'name': 'Sample Portfolio',
+                'week': '6',
+                'link': 'https://github.com/mikeekert/Weekend6-Portfolio',
+                'image': '/assets/images/week6/image0.png',
+                'desc': 'Initial personal portfolio page created for weekend assignment. Utilized the Git' +
+                        'Hub API.',
+                'featured': null
+            }, {
+                'name': 'QuestLog',
+                'week': 'Solo Project',
+                'link': 'https://github.com/mikeekert/soloproject',
+                'link2': 'http://questlog.mikeekert.com/',
+                'image': '/assets/images/solo/image0.png',
+                'desc': 'Personal project created to track video game progress in personal collection. Fu' +
+                        'll CSS3 coding done without any addtional libraries.Retro style themes.',
+                'featured': null
+
+            }, {
+                'name': 'MusicNerdz',
+                'week': 'Group Project',
+                'link': 'https://github.com/ericbfriday/MusicNerdz',
+                'image': '/assets/images/group/image0.png',
+                'desc': 'Group project for Prime Academy. Completed with a team of 5 developers, the appl' +
+                        'ication was created to enhance History curriculum in the classroom by fusing mus' +
+                        'ic and history together. Application allows for admin staff to create content,Te' +
+                        'achers to assign homework, and for Students to take the lessons.',
+                'featured': null
+
+            }
+        ];
     });
