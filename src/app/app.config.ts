fb7f87aa7584788d98aac33252c3aeb5ded6b3bc
mekert <mekert@nrtwebservices.com>
2023-07-31 16:04:15 -0500

    attempt at preloading via angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {PreloadAllModules, provideRouter, withPreloading} from '@angular/router';
 
 import { routes } from './app.routes';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule) ]
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), importProvidersFrom(BrowserAnimationsModule) ]
 };
